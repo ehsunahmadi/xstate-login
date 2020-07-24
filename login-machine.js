9ed18be529dbaaf9605d334766c90462f8848a84
ehsan ahmadi <ehsun.ahmadi@gmail.com>
2020-07-24 18:28:04 +0430

    rewritted everything

--- login-machine.js
+++ login-machine.js
@@ -1,97 +1,157 @@
 import { createMachine, interpret, assign } from "xstate";
 
-const loginMachine = createMachine({
-  initial: "enterMobile",
-  states: {
-    enterMobile: {
-      on: {
-        REQUEST_CODE: "enterCode",
-      },
-    },
-    enterCode: {
-      on: {
-        CHECK_CODE: "enterName",
-      },
-    },
-    enterName: {
-      on: {
-        REGISTER: "loggedIn",
-      },
-    },
-    loggedIn: {},
-  },
-});
+const randomFetch = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
+        rej("Fetch failed!");
+      } else {
+        res("Fetch succeeded!");
+      }
+    }, 2000);
+  });
+};
 
-const loginService = interpret(loginMachine);
+const authMachine = createMachine(
+  {
+    id: "authMachine",
+    initial: "checkingAuth",
+    context: {
+      mobileNumber: undefined,
+      code: undefined,
+      authToken: undefined,
+      nickname: undefined,
+    },
+    states: {
+      checkingAuth: {
+        on: {
+          "": [
+            {
+              target: "authorized",
+              cond: "isAuthorized",
+            },
+            {
+              target: "unauthorized",
+            },
+          ],
+        },
+      },
+      unauthorized: {
+        initial: "enterMobile",
+        id: "login",
+        states: {
+          enterMobile: {
+            on: {
+              REQUEST_CODE: {
+                target: "checkingMobile",
+                actions: "setMobileNumber",
+              },
+            },
+          },
+          checkingMobile: {
+            invoke: {
+              src: "randomFetch",
+              onDone: "enterCode",
+              onError: "enterCode",
+            },
+          },
+          enterCode: {
+            on: {
+              CHECK_CODE: {
+                target: "checkingCode",
+                actions: "setCode",
+              },
+            },
+          },
+          checkingCode: {
+            invoke: {
+              src: "randomFetch",
+              onDone: "enterNickname",
+              onError: "enterNickname",
+            },
+          },
+          enterNickname: {
+            on: {
+              REGISTER: { target: "registering", actions: "setNickname" },
+            },
+          },
+          registering: {
+            invoke: {
+              src: "randomFetch",
+              onDone: "loggingIn",
+              onError: "loggingIn",
+            },
+          },
+          loggingIn: {
+            invoke: {
+              src: "randomFetch",
+              onDone: "success",
+              onError: "err",
+            },
+          },
+          success: { type: "final" },
+          err: { type: "final" },
+        },
+        onDone: "authorized",
+      },
+      authorized: { type: "final" },
+    },
+  },
+  {
+    services: {
+      randomFetch: () => {
+        return randomFetch;
+      },
+    },
+    actions: {
+      setMobileNumber: assign((context, event) => ({
+        mobileNumber: event.payload.mobileNumber,
+      })),
+      setCode: assign((context, event) => ({
+        code: event.payload.code,
+        authToken: event.payload.authToken,
+      })),
+      setNickname: assign((context, event) => ({
+        nickname: event.payload.nickname,
+      })),
+    },
+    guards: {
+      isAuthorized: (context, event) => {
+        return JSON.parse(!!localStorage.getItem("refreshToken"));
+      },
+    },
+  }
+);
+
+const loginService = interpret(authMachine);
 
 loginService.onTransition((state) => {
-  console.log(state.value);
+  console.log(state.value, state.context);
 });
 
 loginService.start();
 
 loginService.send({
   type: "REQUEST_CODE",
+  payload: {
+    mobileNumber: "09901157601",
+  },
 });
 
-loginService.stop();
-
-// --------------------------------------------
-
-const res = fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-
-const fetchCodeMachine = createMachine(
-  {
-    initial: "idle",
-    context: {
-      code: "",
-      retries: 0,
+setTimeout(() => {
+  loginService.send({
+    type: "CHECK_CODE",
+    payload: {
+      code: "12345",
+      authToken: "agfuyt2i23r78",
     },
-    states: {
-      idle: {
-        on: {
-          FETCH_CODE: "loading",
-        },
-      },
-      loading: {
-        on: {
-          REJECT: {
-            target: "failure",
-          },
-          RESOLVE: {
-            actions: assign({
-              code: (context, event) => "23",
-            }),
-            target: "success",
-          },
-        },
-      },
-      failure: {
-        on: {
-          RETRY: {
-            target: "loading",
-            actions: assign({
-              retries: (context, event) => context.retries + 1,
-            }),
-          },
-        },
-      },
-      success: {
-        entry: "goToDashboard",
-      },
+  });
+}, 3000);
+setTimeout(() => {
+  loginService.send({
+    type: "REGISTER",
+    payload: {
+      nickname: "ali",
     },
-  },
-  {
-    actions: {
-      // fetchCode: assign({
-      //   code: (context, event) => "23",
-      // }),
-
-      goToDashboard: () => {
-        console.log("hello dashboard!");
-      },
-    },
-  }
-);
+  });
+}, 6000);
